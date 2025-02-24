8143541bca1590dd4f2084d3560c93110d4c49ba
httpsinisha <sinisajelisavac5@gmail.com>
2025-02-24 16:54:37 +0100

    generisanje populacije_test

--- zad1.py
+++ zad1.py
@@ -1,3 +1,5 @@
+import random
+
 def validan_bin(bin, dimenzije=2):
     zbir = [0] * dimenzije
     for stavka in bin:
@@ -11,3 +13,33 @@ print(validan_bin(bin_test))  # vratice True
 
 bin_test2 = [(0.4, 0.6), (0.7, 0.5)]  # (1.1, 1.1) - nije validno
 print(validan_bin(bin_test2))  # vraca False
+
+
+def generisi_slucajnu_populaciju(stavke, dimenzije=2, velicina_populacije=10):
+    populacija = []
+    
+    for _ in range(velicina_populacije):
+        random.shuffle(stavke)
+        resenje = []
+        
+        for stavka in stavke:
+            dodato = False
+            for bin in resenje:
+                if validan_bin(bin + [stavka], dimenzije):
+                    bin.append(stavka)
+                    dodato = True
+                    break
+            if not dodato:
+                resenje.append([stavka])
+        
+        populacija.append(resenje)
+    
+    return populacija
+
+if __name__ == "__main__":
+    stavke = [(0.3, 0.4), (0.5, 0.2), (0.4, 0.6), (0.2, 0.3), (0.6, 0.5)]
+    
+    populacija = generisi_slucajnu_populaciju(stavke, dimenzije=2, velicina_populacije=5)
+
+    for i, resenje in enumerate(populacija):
+        print(f"Rješenje {i+1}: {resenje}")
